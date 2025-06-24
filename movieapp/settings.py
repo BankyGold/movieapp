8e30e910ef54d883ee6120b838b13b603025b8d4
BankyGold <bankygold7904@gmail.com>
2025-06-24 01:05:14 +0100

    a60

--- movieapp/settings.py
+++ movieapp/settings.py
@@ -118,23 +118,23 @@ SCOREBAT_ACCESS_TOKEN = 'MjA1NDMwXzE3NDMzODM4NjZfM2RlODkyNzk2M2JiYjE0YTE5NDg1Yzl
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'postgres',
-#         'USER': 'postgres',
-#         'PASSWORD': 'Ty1SLsYzbpIan3ddU4qfakL5MDJxMS8UpLWCyib7Rm7rO2DqWa6fFyMlHwyKOo7f',
-#         'HOST': 'j4swkk0og880cc48wswcc0gg',
-#         'PORT': '5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'Ty1SLsYzbpIan3ddU4qfakL5MDJxMS8UpLWCyib7Rm7rO2DqWa6fFyMlHwyKOo7f',
+        'HOST': 'j4swkk0og880cc48wswcc0gg',
+        'PORT': '5432',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
